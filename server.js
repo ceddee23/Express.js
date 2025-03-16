bc4e382ff65859294fcdaeb510cf3cf2bbc814b0
ceddee23 <131221678+ceddee23@users.noreply.github.com>
2025-03-16 19:49:43 +0800

    Added dynamic blog page

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes for different pages
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// Route to fetch blog posts
+app.get('/api/posts', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading posts');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+// Serve the blog page
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
